4fa838c3d71906be6359587c9e210b661e93e0a9
Elad Ossadon <elado7@gmail.com>
2016-07-21 10:28:59 -0700

    resolve_type for EntityInterface

--- app/graph/types/entity_interface.rb
+++ app/graph/types/entity_interface.rb
@@ -1,3 +1,8 @@
+ENTITY_TYPES = {
+  aaa: EntityAaaType,
+  bbb: EntityBbbType,
+}
+
 EntityInterface = GraphQL::InterfaceType.define do
   name 'EntityInterface'
   description '..'
@@ -5,4 +10,8 @@ EntityInterface = GraphQL::InterfaceType.define do
   global_id_field :id
   field :name, !types.String
   field :entityType, !types.String, property: :entity_type
+
+  resolve_type ->(object, ctx) {
+    ENTITY_TYPES[object.entity_type.to_sym]
+  }
 end
